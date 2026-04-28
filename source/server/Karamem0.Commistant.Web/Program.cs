f4a68d5a35df866c484bdb761098bd086e9ac43b
Takashi Shinohara <takashi.shinohara@outlook.com>
2026-04-28 09:29:46 +0900

    5.1.2 release

--- source/server/Karamem0.Commistant.Web/Program.cs
+++ source/server/Karamem0.Commistant.Web/Program.cs
@@ -6,6 +6,8 @@
 // https://github.com/karamem0/commistant/blob/main/LICENSE
 //
 
+#pragma warning disable IDE0053
+
 using Karamem0.Commistant;
 using Microsoft.Agents.Builder;
 using Microsoft.Agents.Hosting.AspNetCore;
@@ -17,14 +19,18 @@ using Microsoft.Extensions.Hosting;
 using System.Threading;
 
 var builder = WebApplication.CreateBuilder(args);
-
 var configuration = builder.Configuration;
+var services = builder.Services;
 
-builder.AddAgent(builder.Configuration);
+_ = builder.WebHost.ConfigureKestrel(options =>
+    {
+        options.AddServerHeader = false;
+    }
+);
+
+builder.AddAgent(configuration);
 builder.AddAzureOpenAIClient(configuration);
 builder.AddAzureBlobContainerClient(configuration);
-
-var services = builder.Services;
 
 _ = services.AddHttpClient();
 _ = services.AddApplicationInsightsTelemetry();
@@ -37,7 +43,6 @@ _ = services.AddRoutes();
 _ = services.AddServices(configuration);
 
 var app = builder.Build();
-
 if (app.Environment.IsDevelopment())
 {
     _ = app.UseDeveloperExceptionPage();
@@ -46,7 +51,21 @@ _ = app.UseHttpsRedirection();
 _ = app.UseHsts();
 _ = app.UseStaticFiles();
 _ = app.MapFallbackToFile("/index.html");
-
+_ = app.Use(async (context, next) =>
+    {
+        var headers = context.Response.Headers;
+        headers.ContentSecurityPolicy = "default-src 'self'; " +
+                                        "connect-src 'self' *.azure.com *.microsoft.com *.office.net; " +
+                                        "frame-ancestors 'self' *.cloud.microsoft *.microsoft365.com *.office.com teams.microsoft.com; " +
+                                        "img-src 'self' blob: data:; " +
+                                        "style-src 'self' 'unsafe-inline'";
+        headers.XContentTypeOptions = "nosniff";
+        headers.XFrameOptions = "ALLOW-FROM https://teams.microsoft.com/";
+        headers["Permissions-Policy"] = "camera=(), fullscreen=(), geolocation=(), microphone=()";
+        headers["Referrer-Policy"] = "same-origin";
+        await next();
+    }
+);
 _ = app
     .MapPost(
         "/api/messages",
@@ -66,3 +85,5 @@ _ = app
     .RequireAuthorization();
 
 await app.RunAsync();
+
+#pragma warning restore IDE0053
